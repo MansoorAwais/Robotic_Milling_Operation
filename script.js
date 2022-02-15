16967214b2834b2ec8f3461567b41773294b2413
Mansoor Awais <57528424+MansoorAwais@users.noreply.github.com>
2022-02-15 22:33:21 +0100

    v1

---
+++ script.js
@@ -0,0 +1,193 @@
+// Import libraries
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.126.0/build/three.module.js";
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.126.0/examples/jsm/controls/OrbitControls.js";
+import rhino3dm from "https://cdn.jsdelivr.net/npm/rhino3dm@7.11.1/rhino3dm.module.js";
+import { RhinoCompute } from "https://cdn.jsdelivr.net/npm/compute-rhino3d@0.13.0-beta/compute.rhino3d.module.js";
+import { Rhino3dmLoader } from "https://cdn.jsdelivr.net/npm/three@0.124.0/examples/jsm/loaders/3DMLoader.js";
+
+const definitionName = "op.gh";
+
+// Set up sliders
+const path_slider = document.getElementById("path");
+path_slider.addEventListener("mouseup", onSliderChange, false);
+path_slider.addEventListener("touchend", onSliderChange, false);
+
+const s_slider = document.getElementById("s");
+s_slider.addEventListener("mouseup", onSliderChange, false);
+s_slider.addEventListener("touchend", onSliderChange, false);
+
+const loader = new Rhino3dmLoader();
+loader.setLibraryPath("https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/");
+
+let rhino, definition, doc;
+rhino3dm().then(async (m) => {
+  console.log("Loaded rhino3dm.");
+  rhino = m; // global
+
+  //RhinoCompute.url = getAuth( 'RHINO_COMPUTE_URL' ) // RhinoCompute server url. Use http://localhost:8081 if debugging locally.
+  //RhinoCompute.apiKey = getAuth( 'RHINO_COMPUTE_KEY' )  // RhinoCompute server api key. Leave blank if debugging locally.
+
+  RhinoCompute.url = "http://localhost:8081/"; //if debugging locally.
+
+  // load a grasshopper file!
+
+  const url = definitionName;
+  const res = await fetch(url);
+  const buffer = await res.arrayBuffer();
+  const arr = new Uint8Array(buffer);
+  definition = arr;
+
+  init();
+  compute();
+});
+
+async function compute() {
+  const param1 = new RhinoCompute.Grasshopper.DataTree("operation_start");
+  param1.append([0], [path_slider.valueAsNumber]);
+
+  const param2 = new RhinoCompute.Grasshopper.DataTree("operation_end");
+  param2.append([0], [s_slider.valueAsNumber]);
+
+  // clear values
+  const trees = [];
+  trees.push(param1);
+  trees.push(param2);
+
+  const res = await RhinoCompute.Grasshopper.evaluateDefinition(
+    definition,
+    trees
+  );
+
+
+  //console.log(res);
+
+  doc = new rhino.File3dm();
+
+  // hide spinner
+  document.getElementById("loader").style.display = "none";
+
+  //decode grasshopper objects and put them into a rhino document
+  for (let i = 0; i < res.values.length; i++) {
+    for (const [key, value] of Object.entries(res.values[i].InnerTree)) {
+      for (const d of value) {
+        const data = JSON.parse(d.data);
+        const rhinoObject = rhino.CommonObject.decode(data);
+        doc.objects().add(rhinoObject, null);
+      }
+    }
+  }
+
+
+
+  // go through the objects in the Rhino document
+
+  let objects = doc.objects();
+  for ( let i = 0; i < objects.count; i++ ) {
+  
+    const rhinoObject = objects.get( i );
+
+
+     // asign geometry userstrings to object attributes
+    if ( rhinoObject.geometry().userStringCount > 0 ) {
+      const g_userStrings = rhinoObject.geometry().getUserStrings()
+      rhinoObject.attributes().setUserString(g_userStrings[0][0], g_userStrings[0][1])
+      // console.log(rhinoObject.attributes().setUserString(g_userStrings[0][0], g_userStrings[0][1]))
+      
+    }
+  }
+
+
+  // clear objects from scene
+  scene.traverse((child) => {
+    if (!child.isLight) {
+      scene.remove(child);
+    }
+  });
+
+  const buffer = new Uint8Array(doc.toByteArray()).buffer;
+  loader.parse(buffer, function (object) {
+
+    // go through all objects, check for userstrings and assing colors
+
+    object.traverse((child) => {
+      if (child.isLine) {
+
+        if (child.userData.attributes.geometry.userStringCount > 0) {
+          
+          //get color from userStrings
+          const colorData = child.userData.attributes.userStrings[0]
+          const col = colorData[1];
+
+          //convert color from userstring to THREE color and assign it
+          const threeColor = new THREE.Color("rgb(" + col + ")");
+          const mat = new THREE.LineBasicMaterial({ color: threeColor });
+          child.material = mat;
+        }
+      }
+    });
+
+    ///////////////////////////////////////////////////////////////////////
+    // add object graph from rhino model to three.js scene
+    scene.add(object);
+
+  });
+}
+
+function onSliderChange() {
+  // show spinner
+  document.getElementById("loader").style.display = "block";
+  compute();
+}
+
+
+// THREE BOILERPLATE //
+let scene, camera, renderer, controls;
+
+function init() {
+  // create a scene and a camera
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0, 0, 0);
+  camera = new THREE.PerspectiveCamera(
+    275,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.z = +150;
+ 
+  // create the renderer and add it to the html
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  // add some controls to orbit the camera
+  controls = new OrbitControls(camera, renderer.domElement);
+
+  // add a directional light
+  const directionalLight = new THREE.DirectionalLight(0xffffff);
+  directionalLight.intensity = 4;
+  scene.add(directionalLight);
+
+  const ambientLight = new THREE.AmbientLight();
+  scene.add(ambientLight);
+
+  animate();
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  animate();
+}
+
+function meshToThreejs(mesh, material) {
+  const loader = new THREE.BufferGeometryLoader();
+  const geometry = loader.parse(mesh.toThreejsJSON());
+  return new THREE.Mesh(geometry, material);
+}
